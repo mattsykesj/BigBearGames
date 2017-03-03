bde7caeaad8ab2b1c47f8f4fb26170d55f4f8c6a
Matt <Mattsykesj@gmail.com>
2017-03-03 17:07:57 +1300

    Attempting to host via azure for the first time

---
+++ BigBearGames/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace BigBearGames.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BigBearGames.Infrastructure.AppIdentityDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BigBearGames.Infrastructure.AppIdentityDbContext";
+        }
+
+        protected override void Seed(BigBearGames.Infrastructure.AppIdentityDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
